2f2c00f648ebcdde372c8d845532cc9c26056c1a
winston kiprop <winstonekip50@gmail.com>
2022-07-21 12:54:05 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
+import React from 'react';
+
+function Keypad(){
+
+    function handleChange(){
+        console.log("Entering password...")
+    }
+    return(
+        <input type="password" onChange={handleChange}/>
     )
 }
 
